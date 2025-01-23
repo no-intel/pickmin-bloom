51c6b55c20a6704662f2aa888b3c50e6f41b44b3
no-int <tkdduf2122@naver.com>
2025-01-24 02:47:55 +0900

    split js

---
+++ src/main/resources/static/js/openlayers.js
@@ -0,0 +1,36 @@
+const getCurrentLocation = async () => {
+    if (!navigator.geolocation) {
+        console.warn("Geolocation은 이 브라우저에서 지원되지 않습니다. 기본 좌표를 사용합니다.");
+        return { latitude: 0, longitude: 0 };
+    }
+    return new Promise((resolve) => {
+        navigator.geolocation.getCurrentPosition(
+            ({ coords: { latitude, longitude } }) => resolve({ latitude, longitude }),
+            () => {
+                console.warn("위치 정보를 가져오는 데 실패했습니다. 기본 좌표를 사용합니다.");
+                resolve({ latitude: 0, longitude: 0 });
+            }
+        );
+    });
+};
+
+const initMap = async () => {
+    const { latitude, longitude } = await getCurrentLocation();
+    console.log(latitude, longitude);
+
+    const view = new ol.View({
+        center: ol.proj.fromLonLat([longitude, latitude]),
+        zoom: 16,
+    });
+
+    return new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM(),
+            })
+        ],
+        view: view,
+    });
+};
+initMap();
