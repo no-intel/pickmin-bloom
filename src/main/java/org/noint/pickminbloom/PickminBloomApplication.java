dd7cc9dcd71a103ce87365b51b0e2ff76e11ea6c
tkdduf212@gmail.com <tkdduf212@gmail.com>
2025-01-23 21:43:45 +0900

    init

---
+++ src/main/java/org/noint/pickminbloom/PickminBloomApplication.java
@@ -0,0 +1,13 @@
+package org.noint.pickminbloom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickminBloomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickminBloomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/noint/pickminbloom/PickminBloomApplicationTests.java
@@ -0,0 +1,13 @@
+package org.noint.pickminbloom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickminBloomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
