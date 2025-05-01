1e9fba07b0c0b44436dceb6aafbf29a5b09926bd
no-int <tkdduf2122@naver.com>
2025-05-02 01:32:37 +0900

    좌표 이미지 렌더링 적용

--- src/main/resources/static/js/openlayers.js
+++ src/main/resources/static/js/openlayers.js
@@ -70,68 +70,40 @@ const findPosts = async (minX, minY, maxX, maxY) => {
 };
 
 const rendPostsImgCard = (posts) => {
+    document.getElementById("post-container").innerHTML = "";
     posts.forEach(post => {
         const outerDiv = document.createElement("div");
-        const card = document.createElement("div");
-        card.className = "card border-left-primary shadow h-100 py-2";
+        outerDiv.className = "card border-left-success shadow h-100 post-card";
 
         const cardBody = document.createElement("div");
-        cardBody.className = "card-body";
+        cardBody.className = "card-body d-flex flex-column justify-content-between p-2 h-100";
 
-        const row = document.createElement("div");
-        row.className = "row no-gutters align-items-center";
-
-        const colText = document.createElement("div");
-        colText.className = "col mr-2";
-
-        const postLabel = document.createElement("div");
-        postLabel.className = "h5 mb-0 font-weight-bold text-gray-800";
-
+        // 메인 이미지
         const postImg = document.createElement("img");
-        postImg.src = post.presignedUrl || "/img/no-img.png"; // 기본값
-        postImg.className = "col-3";
-        postImg.alt = posts.name;
+        postImg.className = "post-img";
+        postImg.src = post.presignedUrl || "/img/no-img.png";
+        postImg.alt = post.name || "none";
+        postImg.className = "img-fluid mb-2";
         postImg.onerror = function () {
             this.onerror = null;
             this.src = "/img/no-img.png";
         };
 
-        const typeLabel = document.createElement("div");
-        typeLabel.className = "col-auto";
-
+        // 타입 아이콘 이미지
         const typeImg = document.createElement("img");
-        typeImg.src = post.type === "빅플" ? "img/flower.png" : (post.type === "버섯" ? "img/mushroom.png" : "img/explorer.png"); // 기본값
-        // typeImg.className = "col-3";
-        typeImg.alt = posts.type;
+        typeImg.className = "post-type-img";
+        typeImg.src = post.type === "빅플"
+            ? "/img/flower.png"
+            : post.type === "버섯"
+                ? "/img/mushroom.png"
+                : "/img/explorer.png";
+        typeImg.alt = post.type || "type";
 
         // 조립
-        colText.appendChild(postLabel);
-        postLabel.appendChild(postImg);
-        typeLabel.appendChild(typeImg);
+        cardBody.appendChild(postImg);
+        cardBody.appendChild(typeImg);
+        outerDiv.appendChild(cardBody);
 
-        row.appendChild(colText);
-        row.appendChild(typeLabel);
-
-        cardBody.appendChild(row);
-        card.appendChild(cardBody);
-        outerDiv.appendChild(card);
-        document.getElementById("side-post-img").append(outerDiv);
+        document.getElementById("post-container").appendChild(outerDiv);
     })
-    // <div>
-    //     <div className="card border-left-primary shadow h-100 py-2">
-    //         <div className="card-body">
-    //             <div className="row no-gutters align-items-center">
-    //                 <div className="col mr-2">
-    //                     <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">
-    //                         Earnings (Monthly)
-    //                     </div>
-    //                     <div className="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
-    //                 </div>
-    //                 <div className="col-auto">
-    //                     <i className="fas fa-calendar fa-2x text-gray-300"></i>
-    //                 </div>
-    //             </div>
-    //         </div>
-    //     </div>
-    // </div>
 }
