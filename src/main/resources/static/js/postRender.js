0fd1e87dd39bcc4a8ff8e8bd1d725092a25c1653
no-int <tkdduf2122@naver.com>
2025-05-03 02:54:01 +0900

    하단 카드리스트 html create 방식 수정

--- src/main/resources/static/js/postRender.js
+++ src/main/resources/static/js/postRender.js
@@ -15,40 +15,25 @@ const findPosts = async (minLongitude, minLatitude, maxLongitude, maxLatitude, l
 };
 
 const rendPostsImgCard = (posts) => {
-    document.getElementById("post-container").innerHTML = "";
+    const container = document.getElementById("post-container");
+    container.innerHTML = "";
+
     posts.forEach(post => {
-        const outerDiv = document.createElement("div");
-        outerDiv.className = "card border-left-success shadow h-100 post-card";
+        const imageSrc = post.presignedUrl || "/img/no-img.png";
+        const typeIcon = post.type === "빅플" ? "/img/flower.png" : (post.type === "버섯" ? "/img/mushroom.png" : "/img/explorer.png");
 
-        const cardBody = document.createElement("div");
-        cardBody.className = "card-body d-flex flex-column justify-content-between p-2 h-100";
+        const cardHTML = `
+            <div class="card border-left-success shadow h-100 post-card">
+                <div class="card-body d-flex flex-column justify-content-between p-2 h-100">
+                    <img src="${imageSrc}" 
+                         class="post-img img-fluid mb-2" 
+                         alt="${post.name || 'none'}"
+                         onerror="this.onerror=null; this.src='/img/no-img.png';" />
+                    <img src="${typeIcon}" class="post-type-img" alt="${post.type || 'type'}" />
+                </div>
+            </div>
+        `;
 
-        // 메인 이미지
-        const postImg = document.createElement("img");
-        postImg.className = "post-img";
-        postImg.src = post.presignedUrl || "/img/no-img.png";
-        postImg.alt = post.name || "none";
-        postImg.className = "img-fluid mb-2";
-        postImg.onerror = function () {
-            this.onerror = null;
-            this.src = "/img/no-img.png";
-        };
-
-        // 타입 아이콘 이미지
-        const typeImg = document.createElement("img");
-        typeImg.className = "post-type-img";
-        typeImg.src = post.type === "빅플"
-            ? "/img/flower.png"
-            : post.type === "버섯"
-                ? "/img/mushroom.png"
-                : "/img/explorer.png";
-        typeImg.alt = post.type || "type";
-
-        // 조립
-        cardBody.appendChild(postImg);
-        cardBody.appendChild(typeImg);
-        outerDiv.appendChild(cardBody);
-
-        document.getElementById("post-container").appendChild(outerDiv);
-    })
+        container.insertAdjacentHTML('beforeend', cardHTML);
+    });
 }
