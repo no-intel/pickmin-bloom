e1edc897c0b7977f750008ff108de152d5b98d66
no-int <tkdduf2122@naver.com>
2025-05-06 08:30:48 +0900

    로그아웃 추가

--- src/main/java/org/noint/pickminbloom/home/HomeController.java
+++ src/main/java/org/noint/pickminbloom/home/HomeController.java
@@ -1,13 +1,24 @@
 package org.noint.pickminbloom.home;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model, @AuthenticationPrincipal OAuth2User user) {
+        if (user != null) {
+            model.addAttribute("name", user.getAttribute("name"));
+            model.addAttribute("email", user.getAttribute("email"));
+            model.addAttribute("picture", user.getAttribute("picture"));
+            model.addAttribute("isLogin", true);
+        }else {
+            model.addAttribute("isLogin", false);
+        }
         return "index";
     }
 }
--- src/main/java/org/noint/pickminbloom/member/controller/SignController.java
+++ src/main/java/org/noint/pickminbloom/member/controller/SignController.java
@@ -1,5 +1,8 @@
 package org.noint.pickminbloom.member.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,5 +12,11 @@ public class SignController {
     @GetMapping("/sign")
     public String sign() {
         return "/sign";
+    }
+
+    @GetMapping("/sign/out")
+    public String signOut(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, null);
+        return "redirect:/";
     }
 }
