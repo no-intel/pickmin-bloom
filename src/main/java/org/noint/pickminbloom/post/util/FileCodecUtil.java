b7477cc4a3b5db5b462bc62e81cfa86402a73047
no-int <tkdduf2122@naver.com>
2025-05-06 20:59:39 +0900

    이미지 파일 인코딩 추가

---
+++ src/main/java/org/noint/pickminbloom/post/util/FileCodecUtil.java
@@ -0,0 +1,33 @@
+package org.noint.pickminbloom.post.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.noint.pickminbloom.exception.post.EmptyFaillException;
+import org.noint.pickminbloom.exception.post.EncodeFailException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Base64;
+
+@Slf4j
+@Component
+public class FileCodecUtil {
+
+    public String encode(MultipartFile file) {
+        try {
+            if (file == null || file.isEmpty()) { // 빈 파일 또는 null 확인
+                throw new EmptyFaillException();
+            }
+
+            byte[] fileBytes = file.getBytes();
+            return Base64.getEncoder().encodeToString(fileBytes);
+        } catch (IOException e) {
+            log.error("Base64 encode error: {}", e.getMessage());
+            throw new EncodeFailException();
+        }
+    }
+
+    private byte[] decode(String base64) {
+        return Base64.getDecoder().decode(base64);
+    }
+}
---
+++ src/test/java/org/noint/pickminbloom/post/util/FailCodecTest.java
@@ -0,0 +1,55 @@
+package org.noint.pickminbloom.post.util;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.noint.pickminbloom.exception.post.EmptyFaillException;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.util.Base64;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FailCodecTest {
+
+    private FileCodecUtil fileCodecUtil;
+
+    @BeforeEach
+    void setUp() {
+        fileCodecUtil = new FileCodecUtil();
+    }
+
+
+    @Test
+    void 이미지_파일_인코딩() throws Exception {
+        // given
+        byte[] fileContent = new byte[]{1, 2, 3, 4, 5};
+        MockMultipartFile mockMultipartFile = new MockMultipartFile(
+                "file",
+                "sample-image.png",
+                "image/png",
+                fileContent
+        );
+
+        // when
+        String encodedString = fileCodecUtil.encode(mockMultipartFile);
+
+        // then
+        String expectedEncodedString = Base64.getEncoder().encodeToString(fileContent);
+        assertNotNull(encodedString);
+        assertEquals(expectedEncodedString, encodedString);
+    }
+
+    @Test
+    void 이미지_파일_인코딩_실패() throws Exception {
+        // given
+        MockMultipartFile mockMultipartFile = new MockMultipartFile(
+                "file",
+                "empty-image.png",
+                "image/png",
+                new byte[0]
+        );
+
+        // when&then
+        assertThrows(EmptyFaillException.class, () -> fileCodecUtil.encode(mockMultipartFile));
+    }
+}
