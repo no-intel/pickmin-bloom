42f2067479bbec379e502897466bff6b9d28773f
no-int <tkdduf2122@naver.com>
2025-05-15 08:05:14 +0900

    메인페이지 버전 추가 및 공지 추가

--- src/main/java/org/noint/pickminbloom/home/HomeController.java
+++ src/main/java/org/noint/pickminbloom/home/HomeController.java
@@ -1,5 +1,6 @@
 package org.noint.pickminbloom.home;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    @Value("${version}")
+    private String version;
 
     @GetMapping("/")
     public String home(Model model, @AuthenticationPrincipal OAuth2User user) {
+        model.addAttribute("version", version);
         if (user != null) {
             model.addAttribute("name", user.getAttribute("name"));
             model.addAttribute("email", user.getAttribute("email"));
